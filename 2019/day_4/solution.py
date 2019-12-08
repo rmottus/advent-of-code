8b4800a088f2b65a2a78a815dcbe7d33995e7ac5
rmottus <rmottus@live.ca>
2019-12-08 14:08:24 -0800

    Finished day 4 by generating all results

--- 2019/day_4/solution.py
+++ 2019/day_4/solution.py
@@ -1,4 +1,17 @@
-import sys
+import sys, itertools
+
+def check_adj_matching_digits(num: int):
+    digits = [ d for d in str(num) ]
+    cur_len = 1
+    for i in range (len(digits) - 1):
+        if digits[i] == digits[i+1]:
+            cur_len += 1
+        else:
+            if cur_len == 2:
+                return True
+            cur_len = 1
+
+    return cur_len == 2
 
 def main():
     [min, max] = [int(i) for i in sys.argv[1:3]]
@@ -28,6 +41,40 @@ def main():
     #       so we are effectively working in base 9
     # Then there are 9 * 10/2 * 11/3 * 12/4 * 13/5 * 14/6 - 8* 9/2 * 10/3 * 11/4 * 12/5 * 13/6 = 1287 total passwords with length 6
     # But, how many are between the inputs?
+    # There should be some way to count this without actually generating number, but I'm not going to spend too long thinking about it
+
+    max_exp = len(str(max))
+    # Wasting a bit of memory here by having an empty 0th index to make the logic a bit easier to think about
+    # since none of the numbers can start with 0 or contain a 0
+    non_dec = [
+        [
+            [] for j in range(10)
+        ] for i in range(max_exp)
+    ]
+    inc = [[[] for j in range(10)] for i in range(max_exp)]
+
+    for i in range(1, 10):
+        non_dec[0][i].append(i)
+        inc[0][i].append(i)
+
+    for exp in range(1, max_exp):
+        for first_digit in range(1, 10):
+            to_add = first_digit * (10 ** exp)
+            for next_digit in range(first_digit, 10):
+                for num in non_dec[exp-1][next_digit]:
+                    non_dec[exp][first_digit].append(to_add + num)
+                if next_digit > first_digit:
+                    for num in inc[exp-1][next_digit]:
+                        inc[exp][first_digit].append(to_add + num)
+
+    final_non_dec = list(itertools.chain.from_iterable(non_dec[-1]))
+    final_inc = list(itertools.chain.from_iterable(inc[-1]))
+
+    result = [ i for i in final_non_dec if i not in final_inc and i > min and i < max ]
+    print(f"Count of non-decreasing numbers with at least one repeated consecutive digit between {min} and {max}: {len(result)}")
+
+    result_part_2 = [ i for i in result if check_adj_matching_digits(i) ]
+    print(f"Count of non-decreasing numbers with at least one repeated consecutive digit that's not a part of a larger group of matching digits between {min} and {max}: {len(result_part_2)}")
 
 if __name__ == '__main__':
     main()
